383a302871e7d0ec06b42fedcb731f142d86cea9
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-04 22:25:57 +0700

    Create jhgi.js

---
+++ jhgi.js
@@ -0,0 +1,25 @@
+function FindProxyForURL(url, host) {
+	if (dnsDomainIs(host, "ocsp.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (dnsDomainIs(host, "mesu.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (dnsDomainIs(host, "gdmf.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+		isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
+		isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")) {
+		return "DIRECT";
+	}
+	
+	if (isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0")) {
+		return "DIRECT";
+	}
+	
+	return "DIRECT";
+}
