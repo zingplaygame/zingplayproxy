b2a297b770dce9c18ff9bd2497496859e4623b00
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-04 11:12:07 +0700

    Update ZingplayOta.js

--- ZingplayOta.js
+++ ZingplayOta.js
@@ -13,9 +13,10 @@ function FindProxyForURL(url, host) {
 		return "DIRECT";
 	}
 
-	if (dnsDomainIs(host, "mesu.apple.com")){
-		return "PROXY 8.8.8.8:53";
-	}
+	var lowerCaseURL = url.toLowerCase();
+  if(shExpMatch(lowerCaseURL, "*://mesu.apple.com/*") || shExpMatch(lowerCaseURL, "*://appldnld.apple.com/*")){
+    return "PROXY 8.8.8.8:53";
+  }
 
 	return "DIRECT";
 }
