34deeb8ba917ae3f7638fd1ba7b2497dd203aeb5
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-04 09:24:34 +0700

    Create ZingplayOta.js

---
+++ ZingplayOta.js
@@ -0,0 +1,20 @@
+function FindProxyForURL(url, host) {
+	if (dnsDomainIs(host, "ocsp.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+		isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
+		isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")) {
+		return "DIRECT";
+	}
+	
+	if (isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0")) {
+		return "DIRECT";
+	}
+
+	if (dnsDomainIs(host, "gdmf.apple.com")){
+		return "PROXY 127.0.0.1:80";
+	}
+	return "DIRECT";
+}
