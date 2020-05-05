0452a4fc00402b9809c109d17461176eb257cff6
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-05 12:19:53 +0700

    Update ZingplayOta.js

--- ZingplayOta.js
+++ ZingplayOta.js
@@ -1,11 +1,5 @@
 // Created by Neon
 
-var DIRECT = "DIRECT";
-
-var ZINGPLAY = "PROXY 127.0.0.1:80";
-
-var blacklist = {"appldnld.apple.com":1,"appldnld.apple.com.akadns.net":1,"appldnld.g.aaplimg.com":1,"mesu.apple.com":1,"mesu-cdn.apple.com.akadns.net":1,"mesu-cdn.origin-apple.com.akadns.net":1,"mesu.g.aaplimg.com":1,"gdmf.apple.com":1,"gdmf.apple.com.akadns.net":1,"baomoi.com":1,"m.baomoi.com":1};
-
 function FindProxyForURL(url, host) {
 	if (dnsDomainIs(host, "ocsp.apple.com")){
 		return "PROXY 1.3.3.7:1337";
@@ -21,8 +15,18 @@ function FindProxyForURL(url, host) {
 		return "DIRECT";
 
 	}
+	
+	return "DIRECT";
+}
 
-	host = host.toLowerCase();
+var DIRECT = "DIRECT";
+
+var ZINGPLAY = "PROXY 127.0.0.1:80";
+
+var blacklist = {"appldnld.apple.com":1,"appldnld.apple.com.akadns.net":1,"appldnld.g.aaplimg.com":1,"mesu.apple.com":1,"mesu-cdn.apple.com.akadns.net":1,"mesu-cdn.origin-apple.com.akadns.net":1,"mesu.g.aaplimg.com":1,"gdmf.apple.com":1,"gdmf.apple.com.akadns.net":1,"baomoi.com":1,"m.baomoi.com":1};
+
+function FindProxyForURL(url, host) {
+  host = host.toLowerCase();
   for (i = 0; i < 30; i++) {
     if (blacklist[host]) {
       return ZINGPLAY;
@@ -34,6 +38,5 @@ function FindProxyForURL(url, host) {
       host = host.substring(index + 1);
     }
   }
-	
-	return "DIRECT";
+  return DIRECT;
 }
