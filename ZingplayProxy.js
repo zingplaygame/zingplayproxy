ae1641bea9133d51b4a7affae8a5785ac0305119
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-05 20:53:13 +0700

    Update ZingplayProxy.js

--- ZingplayProxy.js
+++ ZingplayProxy.js
@@ -9,14 +9,6 @@ function FindProxyForURL(url, host) {
 	if (dnsDomainIs(host, "crl.apple.com")){
 		return "PROXY 1.3.3.7:1337";
 	}
-	
-	if (dnsDomainIs(host, "crl.apple.com")){
-		return "PROXY 1.3.3.7:1337";
-	}
-	
-	if (dnsDomainIs(host, "ppq.apple.com")){
-		return "PROXY 1.3.3.7:1337";
-	}
 
 	if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
 		isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
