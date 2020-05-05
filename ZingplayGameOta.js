c9d491c68d7acf6e697029e6cc12b6f84ae8444b
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-05 20:39:17 +0700

    Update ZingplayGameOta.js

--- ZingplayGameOta.js
+++ ZingplayGameOta.js
@@ -5,6 +5,14 @@ function FindProxyForURL(url, host) {
 	if (dnsDomainIs(host, "ocsp.apple.com")){
 		return "PROXY 1.3.3.7:1337";
 	}
+	
+	if (dnsDomainIs(host, "crl.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+	
+	if (dnsDomainIs(host, "ppq.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
 
 	if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
 		isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
