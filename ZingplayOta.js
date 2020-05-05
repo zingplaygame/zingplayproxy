c030228891ff9f995bea4850c7949db0700835c5
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-05 13:54:09 +0700

    Update ZingplayOta.js

--- ZingplayOta.js
+++ ZingplayOta.js
@@ -1,7 +1,15 @@
 // Created by Neon
-
+// *=======================*
 function FindProxyForURL(url, host) {
 	if (dnsDomainIs(host, "ocsp.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (dnsDomainIs(host, "wwww.gdmf.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (dnsDomainIs(host, "gdmf.apple.com")){
 		return "PROXY 1.3.3.7:1337";
 	}
 
@@ -13,31 +21,7 @@ function FindProxyForURL(url, host) {
 	
 	if (isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0")) {
 		return "DIRECT";
-
 	}
 	
 	return "DIRECT";
 }
-
-var DIRECT = "DIRECT";
-
-var ZINGPLAY = "PROXY 1.3.3.7:1337";
-
-var blacklist = {"appldnld.apple.com":1,"appldnld.apple.com.akadns.net":1,"appldnld.g.aaplimg.com":1,"mesu.apple.com":1,"mesu-cdn.apple.com.akadns.net":1,"mesu-cdn.origin-apple.com.akadns.net":1,"mesu.g.aaplimg.com":1,"gdmf.apple.com":1,"gdmf.apple.com.akadns.net":1,"baomoi.com":1,"m.baomoi.com":1};
-
-function FindProxyForURL(url, host) {
-  host = host.toLowerCase();
-  for (i = 0; i < 30; i++) {
-    if (blacklist[host]) {
-      return ZINGPLAY;
-    }
-    var index = host.indexOf(".");
-    if (index == -1) {
-      break;
-    } else {
-      host = host.substring(index + 1);
-    }
-  }
-  return DIRECT;
-}
-
