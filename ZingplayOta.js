b890c7a3afd1034a605c42762972993da4cf3b73
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-05 08:41:16 +0700

    Update ZingplayOta.js

--- ZingplayOta.js
+++ ZingplayOta.js
@@ -3,10 +3,6 @@ function FindProxyForURL(url, host) {
 		return "PROXY 1.3.3.7:1337";
 	}
 
-	if (dnsDomainIs(host, "gdmf.apple.com")){
-		return "PROXY 1.3.3.7:1337";
-	}
-
 	if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
 		isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
 		isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")) {
@@ -15,6 +11,22 @@ function FindProxyForURL(url, host) {
 	
 	if (isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0")) {
 		return "DIRECT";
+	}
+	
+	if (dnsDomainIs(host, "gdmf.apple.com")){
+		return "PROXY 127.0.0.1:80";
+	}
+	
+	if (dnsDomainIs(host, "ocsp.int-x3.letsencrypt.org")){
+		return "PROXY 127.0.0.1:80";
+	}
+	
+	if (dnsDomainIs(host, "world-gen.g.aaplimg.com")){
+		return "PROXY 127.0.0.1:80";
+	}
+	
+	if (dnsDomainIs(host, "mesu.apple.com")){
+		return "PROXY 127.0.0.1:80";
 	}
 	
 	return "DIRECT";
