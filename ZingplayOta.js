1e6ad9881cd9b7267cc33d966b1927f5e884e977
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-05 14:09:54 +0700

    Update ZingplayOta.js

--- ZingplayOta.js
+++ ZingplayOta.js
@@ -1,19 +1,12 @@
-// Created by Neon
-// *=======================*
 function FindProxyForURL(url, host) {
-	// Block OCSP :)
 	if (dnsDomainIs(host, "ocsp.apple.com")){
 		return "PROXY 1.3.3.7:1337";
 	}
-
-	if (dnsDomainIs(host, "mesu.apple.com")){
-		return "PROXY 1.3.3.7:1337";
-	}
-
+	
 	if (dnsDomainIs(host, "gdmf.apple.com")){
 		return "PROXY 1.3.3.7:1337";
 	}
-
+	
 	if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
 		isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
 		isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")) {
@@ -22,6 +15,30 @@ function FindProxyForURL(url, host) {
 	
 	if (isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0")) {
 		return "DIRECT";
+	}
+	
+	if (dnsDomainIs(host, "mesu.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+	
+	if (dnsDomainIs(host, "gdmf.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+	
+	if (dnsDomainIs(host, "appldnld.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+	
+	if (dnsDomainIs(host, "ocsp.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+	
+	if (dnsDomainIs(host, "ocsp.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+	
+	if (dnsDomainIs(host, "ocsp.apple.com")){
+		return "PROXY 1.3.3.7:1337";
 	}
 	
 	return "DIRECT";
