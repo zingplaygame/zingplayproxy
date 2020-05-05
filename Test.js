d12d765a51ab6d897a1b3db9007d0915752853b8
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-05 07:14:19 +0700

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,44 @@
+// Created by Neon
+// *=======================*
+// Twitter : @prefisso
+// Website : nenno.1mb.site
+// *=======================*
+
+function FindProxyForURL(url, host) {
+	// Block OCSP :)
+	if (dnsDomainIs(host, "ocsp.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (dnsDomainIs(host, "gdmf.apple.com")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (dnsDomainIs(host, "tinhte.vn")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (dnsDomainIs(host, "m.tinhte.vn")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+		isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
+		isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")) {
+		return "DIRECT";
+	}
+	
+	if (isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0")) {
+		return "DIRECT";
+	}
+
+	if (dnsDomainIs(host, "baomoi.vn")){
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (dnsDomainIs(host, "m.baomoi.vn")){
+		return "PROXY 1.3.3.7:1337";
+	}
+	
+	return "DIRECT";
+}
