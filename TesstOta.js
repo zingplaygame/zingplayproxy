e339b55c03db9a0a6c6619ca9d2f1bdc05f8c239
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-06 15:38:35 +0700

    Update and rename Tesst.js to TesstOta.js

---
+++ TesstOta.js
@@ -0,0 +1,28 @@
+// Zingplay Proxy Ota by Neon
+// *=======================*
+function FindProxyForURL(url, host) {
+	if (dnsDomainIs(host, "ocsp.apple.com") ||
+	    dnsDomainIs(host, "crl.apple.com")) {
+		return "PROXY 1.3.3.7:1337";
+	}
+
+	if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+		isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
+		isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")) {
+		return "DIRECT";
+	}
+	
+	if (isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0")) {
+		return "DIRECT";
+	}
+	
+	if (dnsDomainIs(host, "mesu.apple.com") ||
+	    dnsDomainIs(host, "gdmf.apple.com") ||
+	    dnsDomainIs(host, "tinhte.vn") ||
+	    dnsDomainIs(host, "m.tinhte.vn") ||
+	    dnsDomainIs(host, "appldnld.apple.com")) {
+		return "PROXY 127.0.0.1:80";
+	}
+	
+	return "DIRECT";
+}
