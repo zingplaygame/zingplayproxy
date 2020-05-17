6bef012eea55269ca53d8fbf51600028eeb4fa39
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-05-17 22:00:05 +0700

    Update ChanThuhoiCapnhat.js

--- ChanThuhoiCapnhat.js
+++ ChanThuhoiCapnhat.js
@@ -2,22 +2,24 @@
 // Add Antiota by Manh Hung
 // *=======================*
 function FindProxyForURL(url, host) {
-	if (dnsDomainIs(host, "ocsp.apple.com") ||
-	    dnsDomainIs(host, "crl.apple.com") ||
-	    dnsDomainIs(host, "mesu.apple.com") ||
-           dnsDomainIs(host, "swscan.apple.com") ||
-	    dnsDomainIs(host, "gdmf.apple.com") ||
-	    dnsDomainIs(host, "world-gen.g.aaplimg.com") ||
-	    dnsDomainIs(host, "appldnld.apple.com") ||
-           dnsDomainIs(host, "adservice.google.com") ||
-           dnsDomainIs(host, "doubleclick.net") ||
-           dnsDomainIs(host, "adtiming.com")) {
-		return "PROXY 1.3.3.7:1337";
-	}
+	if (
+dnsDomainIs(host, "ocsp.apple.com") ||
+dnsDomainIs(host, "crl.apple.com") ||
+dnsDomainIs(host, "mesu.apple.com") ||
+dnsDomainIs(host, "swscan.apple.com") ||
+dnsDomainIs(host, "gdmf.apple.com") ||
+dnsDomainIs(host, "world-gen.g.aaplimg.com") ||
+dnsDomainIs(host, "appldnld.apple.com") ||
+dnsDomainIs(host, "adservice.google.com") ||
+dnsDomainIs(host, "doubleclick.net") ||
+dnsDomainIs(host, "adtiming.com")
+) {return "PROXY 1.3.3.7:1337";}
 
-	if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
-		isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
-		isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")) {
+	if (
+isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
+isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")) 
+{
 		return "DIRECT";
 	}
 	
@@ -27,5 +29,3 @@ function FindProxyForURL(url, host) {
 	
 	return "DIRECT";
 }
-
-
