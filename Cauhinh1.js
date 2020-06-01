7674de3ed8e4270ae6330427ed30f9e78c37f03e
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-06-01 21:37:12 +0700

    Update Cauhinh1.js

--- Cauhinh1.js
+++ Cauhinh1.js
@@ -1,5 +1,4 @@
-// Antirevoke by Neon
-// Add Antiota by Manh Hung
+// Antirevoke by Tui
 // *=======================*
 var anti = {"ocsp.apple.com":1,"crl.apple.com":1,"mesu.apple.com":1,"gdmf.apple.com":1,"www.gdmf.apple.com":1,"iadc.qwape.com":1,"iadsk.apple.com":1,"swscan.apple.com":1,"world-gen.g.aaplimg.com":1,"appldnld.apple.com":1};
 function FindProxyForURL(url, host) {
