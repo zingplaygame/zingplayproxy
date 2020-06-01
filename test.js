bd97ad054e20cb539b50a9dce25c1c823446c8b5
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-06-01 19:49:41 +0700

    Update test.js

--- test.js
+++ test.js
@@ -34,6 +34,6 @@ if (isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0"))
 {return "DIRECT";}
 
 if (blacklist[host]) 
-{return "PROXY 127.0.0.1:80";}
+{return "PROXY 1.3.3.7:1337";}
 
 return "DIRECT";}
