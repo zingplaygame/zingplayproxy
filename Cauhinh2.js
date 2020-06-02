8f040f7818770c52efa8ecad883e02c28e8d266c
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-06-02 09:31:33 +0700

    Update Cauhinh2.js

--- Cauhinh2.js
+++ Cauhinh2.js
@@ -11,13 +11,20 @@ var anti = {
 "swscan.apple.com":1,
 "world-gen.g.aaplimg.com":1,
 "appldnld.apple.com":1,
-"ppq.apple.com":1,
-"tinhte.vn":1,
-"m.tinhte.vn":1,
-"www.tinhte.vn":1};
+"ppq.apple.com":1}; 
 function FindProxyForURL(url, host) {
 if (anti[host]) 
-{return "PROXY 0.0.0.0:0";}
+{return "PROXY 0.0.0.0:0";} 
+var ads = {
+"adservice.google.com":1,
+"doubleclick.net":1,
+"adtiming.com":1,
+"googleadservices.com":1,
+"googlesyndication.com":1,
+"launches.appsflyer.com":1,
+"google-analytics.com":1}; 
+if (ads[host]) 
+{return "PROXY 0.0.0.0:0";} 
 return "DIRECT";}
 
 
