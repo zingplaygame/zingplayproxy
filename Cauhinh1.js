86284ee91b48d05352359cd65fa1470f55079759
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-06-03 12:10:23 +0700

    Update Cauhinh1.js

--- Cauhinh1.js
+++ Cauhinh1.js
@@ -1,4 +1,4 @@
-// Block Revoke-Ota No PPQ
+// Block Revoke-Ota PPQ
 // *=======================*
 function FindProxyForURL(url, host) {
 var anti = {
@@ -13,10 +13,19 @@ var anti = {
 "world-gen.g.aaplimg.com":1,
 "appldnld.apple.com":1}; 
 if (anti[host]) 
+{return "PROXY Timcook; SOCKS5 Timcook; SOCKS Timcook;";} 
+
+var ads = {
+"googleads.g.doubleclick.net":1,
+"adservice.google.com":1,
+"www.google-analytics.com":1,
+"launches.appsflyer.com":1,
+"baomoi.com":1,
+"m.baomoi.com":1} 
+if (ads[host]) 
 {return "PROXY Tim Cook";} 
+
 return "DIRECT";}
-
-
 
 
 
