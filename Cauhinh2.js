fead1de62ebc4884162c1fdc26ff71a6f344cc65
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-06-05 09:38:34 +0700

    Update Cauhinh2.js

--- Cauhinh2.js
+++ Cauhinh2.js
@@ -2,9 +2,18 @@
 // *=======================*
 function FindProxyForURL(url, host) {
 var anti = {
+"ocsp.apple.com":1,
+"crl.apple.com":1,
 "mesu.apple.com":1,
+"gdmf.apple.com":1,
+"www.gdmf.apple.com":1,
+"iadc.qwape.com":1,
 "updates-http.cdn-apple.com":1,
-"gdmf.apple.com":1}; 
+"iadsk.apple.com":1,
+"swscan.apple.com":1,
+"world-gen.g.aaplimg.com":1,
+"ppq.apple.com":1,
+"appldnld.apple.com":1}; 
 if (anti[host]) 
 {return "PROXY Timcook; SOCKS5 Timcook; SOCKS Timcook;";} 
 
@@ -16,7 +25,7 @@ var ads = {
 "baomoi.com":1,
 "m.baomoi.com":1} 
 if (ads[host]) 
-{return "PROXY Tim Cook";} 
+{return "PROXY Timcook";} 
 
 return "DIRECT";}
 
