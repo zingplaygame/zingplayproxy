15e215893800a05c0544043ae2e74edf3dfd509c
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-06-27 18:52:57 +0700

    Update Cauhinh2.js

--- Cauhinh2.js
+++ Cauhinh2.js
@@ -1,29 +1,5 @@
-// Block Ota Update
 function FindProxyForURL(url, host) {
-var anti = {
-"mesu.apple.com":1,
-"gdmf.apple.com":1,
-"updates-http.cdn-apple.com":1}; 
-var lastPos; do {
-if (anti.hasOwnProperty(host)) 
-{return "PROXY Timcook";} 
-lastPos = host.indexOf('.') + 1; 
-host = host.slice(lastPos);} 
-while (lastPos >= 1);
-
+if (dnsDomainIs(host, "mesu.apple.com"))
+    dnsDomainIs(host, "gdmf.apple.com")){
+return "PROXY Timcook";}
 return "DIRECT";}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
