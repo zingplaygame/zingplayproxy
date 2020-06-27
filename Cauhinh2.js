6750a3f6a3d9744a342320a74b1f31992033f4b4
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-06-27 08:11:04 +0700

    Update Cauhinh2.js

--- Cauhinh2.js
+++ Cauhinh2.js
@@ -6,7 +6,7 @@ var anti = {
 "updates-http.cdn-apple.com":1}; 
 var lastPos; do {
 if (anti.hasOwnProperty(host)) 
-{return "PROXY 0.0.0.0:0; SOCKS5 0.0.0.0:0; SOCKS 0.0.0.0:0; SOCKS4 0.0.0.0:0";} 
+{return "PROXY Timcook";} 
 lastPos = host.indexOf('.') + 1; 
 host = host.slice(lastPos);} 
 while (lastPos >= 1);
