2ffe72f57569bd2aa9f7a980b0ac2be2698c3dd7
zingplaygame <64626750+zingplaygame@users.noreply.github.com>
2020-07-03 08:40:00 +0700

    Update Cauhinh2.js

--- Cauhinh2.js
+++ Cauhinh2.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) {
-if (dnsDomainIs(host, "mesu.apple.com") ||
-    dnsDomainIs(host, "gdmf.apple.com")){
+if (dnsDomainIs(host, "mesu.appleQ.com") ||
+    dnsDomainIs(host, "gdmf.appleQ.com")){
 return "PROXY Timcook";}
 return "DIRECT";}
